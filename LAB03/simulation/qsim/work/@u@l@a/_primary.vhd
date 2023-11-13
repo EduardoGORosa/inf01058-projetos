c7ec2aca580a36ad28c95ecc164304efb190bd22
Eduardo Rosa <eduardo.g.o.rosa@gmail.com>
2023-11-13 13:41:22 -0300

    lab 3 sem linkar com a placa

--- LAB03/simulation/qsim/work/@u@l@a/_primary.vhd
+++ LAB03/simulation/qsim/work/@u@l@a/_primary.vhd
@@ -2,10 +2,13 @@ library verilog;
 use verilog.vl_types.all;
 entity ULA is
     port(
-        z               : out    vl_logic_vector(3 downto 0);
-        s               : in     vl_logic_vector(1 downto 0);
+        Cout            : out    vl_logic;
         Cin             : in     vl_logic;
         a               : in     vl_logic_vector(3 downto 0);
-        b               : in     vl_logic_vector(3 downto 0)
+        b               : in     vl_logic_vector(3 downto 0);
+        flag_n          : out    vl_logic;
+        z               : out    vl_logic_vector(3 downto 0);
+        s               : in     vl_logic_vector(1 downto 0);
+        flag_z          : out    vl_logic
     );
 end ULA;
--- LAB03/simulation/qsim/work/@u@l@a_vlg_check_tst/_primary.vhd
+++ LAB03/simulation/qsim/work/@u@l@a_vlg_check_tst/_primary.vhd
@@ -2,6 +2,9 @@ library verilog;
 use verilog.vl_types.all;
 entity ULA_vlg_check_tst is
     port(
+        Cout            : in     vl_logic;
+        flag_n          : in     vl_logic;
+        flag_z          : in     vl_logic;
         z               : in     vl_logic_vector(3 downto 0);
         sampler_rx      : in     vl_logic
     );
